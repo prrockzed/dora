6e265cf1908bf8ef8e09efcbe8194cf2dec5956d
prrockzed <priyanshuranjan2806@gmail.com>
2024-03-22 01:50:30 +0530

    Modified Experience
    Sorted experience by sequence and percentage. Also added key index pair

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,15 @@
-const Experience = () => {
+const Experience = ({ userData }) => {
+  const exp = userData.data.skills;
+  exp.sort((a, b) => {
+    // Compare sequence numbers first
+    if (a.sequence !== b.sequence) {
+      return a.sequence - b.sequence;
+    } else {
+      // If sequence numbers are equal, compare percentages
+      return b.percentage - a.percentage; // Sorting in descending order of percentage
+    }
+  });
+
   return (
     <section className="experience-section" id="about">
       <div className="container">
@@ -8,94 +19,20 @@ const Experience = () => {
           <h2>My Experience Area</h2>
         </div>
         <div className="experience-items wow fadeInUp">
-          {/* 01 experience item start */}
-          <div className="experience-item">
-            <div className="experience-info">
-              <p>Facebook Marketing</p>
-              <p>90%</p>
+          {[...Array(exp.length)].map((_, index) => (
+            <div className="experience-item" key={exp[index]._id}>
+              <div className="experience-info">
+                <p>{exp[index].name}</p>
+                <p>{exp[index].percentage}%</p>
+              </div>
+              <div
+                className="progress-line"
+                data-percent={`${exp[index].percentage}%`}
+              >
+                <span />
+              </div>
             </div>
-            <div className="progress-line" data-percent="90%">
-              <span />
-            </div>
-          </div>
-          {/* 01 experience item end */}
-          {/* 02 experience item start */}
-          <div className="experience-item">
-            <div className="experience-info">
-              <p>Affiliate Marketing</p>
-              <p>50%</p>
-            </div>
-            <div className="progress-line" data-percent="50%">
-              <span />
-            </div>
-          </div>
-          {/* 02 experience item end */}
-          {/* 03 experience item start */}
-          <div className="experience-item">
-            <div className="experience-info">
-              <p>Search Engine Optimization</p>
-              <p>80%</p>
-            </div>
-            <div className="progress-line" data-percent="80%">
-              <span />
-            </div>
-          </div>
-          {/* 03 experience item end */}
-          {/* 04 experience item start */}
-          <div className="experience-item">
-            <div className="experience-info">
-              <p>Graphic Design</p>
-              <p>90%</p>
-            </div>
-            <div className="progress-line" data-percent="90%">
-              <span />
-            </div>
-          </div>
-          {/* 04 experience item end */}
-          {/* 05 experience item start */}
-          <div className="experience-item">
-            <div className="experience-info">
-              <p>Content Writing</p>
-              <p>95%</p>
-            </div>
-            <div className="progress-line" data-percent="95%">
-              <span />
-            </div>
-          </div>
-          {/* 05 experience item end */}
-          {/* 06 experience item start */}
-          <div className="experience-item">
-            <div className="experience-info">
-              <p>Web Ui Design</p>
-              <p>49%</p>
-            </div>
-            <div className="progress-line" data-percent="49%">
-              <span />
-            </div>
-          </div>
-          {/* 06 experience item end */}
-          {/* 07 experience item start */}
-          <div className="experience-item">
-            <div className="experience-info">
-              <p>Youtube Marketing</p>
-              <p>72%</p>
-            </div>
-            <div className="progress-line" data-percent="72%">
-              <span />
-            </div>
-          </div>
-          {/* 07 experience item end */}
-          {/* 08 experience item start */}
-          <div className="experience-item">
-            <div className="experience-info">
-              <p>Web Design</p>
-              <p>70%</p>
-            </div>
-            <div className="progress-line" data-percent="70%">
-              <span />
-            </div>
-          </div>
-          {/* 08 experience item end */}
+          ))}
         </div>
       </div>
     </section>
